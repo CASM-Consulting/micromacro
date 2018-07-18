046bcfb9e2038c282e95341d51e81246b0da7f0f
Simon Wibberley <simon.wibberley@sussex.ac.uk>
2018-07-18 20:16:31 +0100

    wip

---
+++ src/main/java/uk/ac/susx/shl/micromacro/webapp/resources/Method52Resouce.java
@@ -0,0 +1,18 @@
+package uk.ac.susx.shl.micromacro.webapp.resources;
+
+import org.jdbi.v3.core.Jdbi;
+
+/**
+ * Created by sw206 on 18/07/2018.
+ */
+public class Method52Resouce {
+
+
+    public Method52Resouce(Jdbi jdbi) {
+
+    }
+
+
+
+
+}
