0644738d001f455b5df74c79a6fac1262acdb54f
Simon Wibberley <simon.wibberley@gmail.com>
2019-01-21 18:11:59 +0000

    added basic result pagination

--- src/main/resources/assets/js/components/queryResult.js
+++ src/main/resources/assets/js/components/queryResult.js
@@ -7,9 +7,12 @@ MicroMacroApp.component('queryResult', {
     },
     controller : function($scope, $stateParams) {
 
-        var TOKEN_HACK = $scope.TOKEN_HACK = false;
-
         var $ctrl = this;
+
+        $scope.page = [];
+        $scope.currentPage = 1;
+        $scope.numPerPage = 30;
+        $scope.maxSize = 10;
 
         $ctrl.$onInit = function() {
             $scope.selectedKeys = $stateParams.displayKeys || {};
@@ -23,6 +26,13 @@ MicroMacroApp.component('queryResult', {
 
 
             $scope.keyList.sort();
+
+            $scope.$watch('currentPage + numPerPage', function() {
+                var begin = (($scope.currentPage - 1) * $scope.numPerPage);
+                var end = begin + $scope.numPerPage;
+
+                $scope.page = $ctrl.result.slice(begin, end);
+            });
         };
 
         $scope.cols = function(max, num) {
