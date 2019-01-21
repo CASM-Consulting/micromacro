f23ca2e442bd2b3022dd671f82b5aceb604b1103
Simon Wibberley <simon.wibberley@gmail.com>
2019-01-21 15:27:01 +0000

    LabelDecision -> String Map converter

--- src/main/java/uk/ac/susx/shl/micromacro/jdbi/DatumMapper.java
+++ src/main/java/uk/ac/susx/shl/micromacro/jdbi/DatumMapper.java
@@ -6,12 +6,22 @@ import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 import uk.ac.susx.tag.method51.core.gson.GsonBuilderFactory;
 import uk.ac.susx.tag.method51.core.meta.Datum;
+import uk.ac.susx.tag.method51.core.meta.Key;
 import uk.ac.susx.tag.method51.core.meta.KeySet;
+import uk.ac.susx.tag.method51.core.meta.types.RuntimeType;
+import uk.ac.susx.tag.method51.twitter.LabelDecision;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
 
 public class DatumMapper implements RowMapper<Datum> {
+
+    private static final Logger LOG = Logger.getLogger(DatumMapper.class.getName());
 
     public final Gson gson;
 
@@ -27,6 +37,35 @@ public class DatumMapper implements RowMapper<Datum> {
         String json = rs.getString(2);
 
         Datum datum = gson.fromJson(json, Datum.class);
+
+        datum = labelDecision2Label(datum);
+
+        return datum;
+    }
+
+
+    public static Datum labelDecision2Label(Datum datum) {
+
+        Set<String> models = new HashSet<>();
+
+        for(Key key : datum.getKeys().keys.keys()) {
+
+            if(key.type.isAssignableFrom(RuntimeType.of(LabelDecision.class))) {
+
+                LabelDecision l = (LabelDecision)datum.get(key);
+
+                int idx = key.name.lastIndexOf(l.label);
+
+                String model = key.name.substring(0,idx-1);
+
+                if(!models.add(model)) {
+                    LOG.warning("multiple label decisions for " + model);
+                }
+
+                datum = datum.without(key)
+                    .with(Key.of(key.namespace, model, RuntimeType.STRING), l.label);
+            }
+        }
 
         return datum;
     }
---
+++ src/test/java/uk/ac/susx/shl/micromacro/jdbi/DatumMapperTest.java
@@ -0,0 +1,48 @@
+package uk.ac.susx.shl.micromacro.jdbi;
+
+import org.junit.jupiter.api.Test;
+import uk.ac.susx.tag.method51.core.meta.Datum;
+import uk.ac.susx.tag.method51.core.meta.Key;
+import uk.ac.susx.tag.method51.core.meta.types.RuntimeType;
+import uk.ac.susx.tag.method51.twitter.LabelDecision;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatumMapperTest {
+
+    @Test
+    void labelDecision2Label() {
+
+        String ns = "classifier";
+        String model = "model1";
+        String label1 = "label1";
+        String label2 = "label2";
+        String[] labels = new String[]{label1, label1};
+
+        Key<LabelDecision> label1Key = Key.of(ns, model+"-"+label1, RuntimeType.of(LabelDecision.class));
+        Key<LabelDecision> label2Key = Key.of(ns, model+"-"+label2, RuntimeType.of(LabelDecision.class));
+
+        LabelDecision ld1 = new LabelDecision();
+        ld1.label = label1;
+        ld1.labels = labels;
+        ld1.likelihoods = new double[]{1.0,0.0};
+
+        LabelDecision ld2 = new LabelDecision();
+        ld2.label = label2;
+        ld2.labels = labels;
+        ld2.likelihoods = new double[]{0.0,1.0};
+
+        Datum datum1 = new Datum()
+                .with(label1Key, ld1);
+
+        Datum datum2 = new Datum()
+                .with(label2Key, ld2);
+
+        datum1 = DatumMapper.labelDecision2Label(datum1);
+        datum2 = DatumMapper.labelDecision2Label(datum2);
+
+        assertEquals(datum1.get(Key.of(ns, model, RuntimeType.STRING)), "label1");
+        assertEquals(datum2.get(Key.of(ns, model, RuntimeType.STRING)), "label2");
+
+    }
+}
