567d474d6d4426b8183f8a1ae24668577b2885bc
Simon Wibberley <simon.wibberley@gmail.com>
2019-01-23 15:22:27 +0000

    neater undo/redo controls

--- src/main/resources/assets/js/components/queryConfig.js
+++ src/main/resources/assets/js/components/queryConfig.js
@@ -11,15 +11,15 @@ MicroMacroApp.component('queryConfig', {
 
         $ctrl.queryId = $stateParams.queryId;
 
-        $scope.reload = function() {
+        $scope.reload = () => {
 //            $state.reload("workspace.query");
             Tables.schema($ctrl.query.table).then(function(keys) {
                 $ctrl.keys = keys;
             })
         };
 
-        $ctrl.$onInit = function(){
-            if($ctrl.query.table) {
+        $ctrl.$onInit = () => {
+            if(!$ctrl.keys && $ctrl.query.table) {
                 Tables.schema($ctrl.query.table).then(function(keys) {
                     $scope.keys = keys;
                 });
@@ -28,23 +28,25 @@ MicroMacroApp.component('queryConfig', {
             $scope.queryVer = $stateParams.ver;
         };
 
-        $scope.execute = function () {
+        $scope.execute = () => {
             Queries.execute($scope.$ctrl.query).then(function(data){
                 $scope.results = data;
             });
         };
 
-        $scope.addProxy = function() {
+        $scope.addProxy = () => {
             Queries.addProxy($stateParams.workspaceId, $ctrl.queryId, $ctrl.query).then(function(query){
                 alert("saved");
-                $state.go("workspace.query", {workspaceId:$stateParams.workspaceId, queryId:$ctrl.queryId});
+                $state.go(".", {workspaceId:$stateParams.workspaceId, queryId:$ctrl.queryId, ver:0});
+                $scope.queryVer = 0;
             });
         };
 
-        $scope.addSelect = function() {
+        $scope.addSelect = () => {
             Queries.addSelect($stateParams.workspaceId, $ctrl.queryId, $ctrl.query).then(function(query){
                 alert("saved");
-                $state.go("workspace.query", {workspaceId:$stateParams.workspaceId, queryId:$ctrl.queryId});
+                $state.go(".", {workspaceId:$stateParams.workspaceId, queryId:$ctrl.queryId, ver:0});
+                $scope.queryVer = 0;
             });
         };
 
@@ -56,8 +58,10 @@ MicroMacroApp.component('queryConfig', {
         };
 
         $scope.redo = () => {
-            ++$scope.queryVer;
-            $scope.changeVer();
+            if($scope.queryVer < 0) {
+                ++$scope.queryVer;
+                $scope.changeVer();
+            }
         };
 
     }
