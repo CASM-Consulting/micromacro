4a3d0f64132ac907cc0ec08599e89489a9d8e9d7
Simon Wibberley <simon.wibberley@gmail.com>
2019-01-26 19:51:13 +0000

    switched to character index spans, basic coloured []'s

--- src/main/resources/assets/js/components/spanText.js
+++ src/main/resources/assets/js/components/spanText.js
@@ -2,36 +2,16 @@ MicroMacroApp.component('spanText', {
     bindings : {
         spanss: '<',
         text : '<',
-        tokenHack : '<'
+        displayKeys: '<'
     },
     templateUrl: 'html/spanText.html',
-    controller : function() {
+    controller : function(Spans) {
 
         var $ctrl = this;
 
         $ctrl.$onInit = function() {
-            $ctrl.tokens = [];
-            var tokens;
-//            if($ctrl.tokenHack) {
-//                tokens = $ctrl.text.split(' ');
-//            } else {
-                tokens = $ctrl.text;
-//            }
-            for(var i in tokens) {
-                $ctrl.tokens.push({
-                    text:tokens[i]
-                });
-            }
 
-            for(var keyName in $ctrl.spanss) {
-                var spans = $ctrl.spanss[keyName];
-                for(var idx in spans) {
-                    var span = spans[idx];
-                    for(var i = span.from; i < span.to; ++i) {
-                        $ctrl.tokens[i].color = 'red';
-                    }
-                }
-            }
+            $ctrl.segments = Spans.segments($ctrl.text, $ctrl.spanss, $ctrl.displayKeys);
         };
     }
 });
---
+++ src/main/resources/assets/js/services/SpanService.js
@@ -0,0 +1,60 @@
+MicroMacroApp.factory("Spans", function() {
+    var colours = ['red', 'blue', 'green', 'orange', 'grey']
+
+    return {
+        segments : function(text, spanss, displayKeys) {
+            var spanIndices = [];
+
+            for(var i = 0; i < text.length; ++i) {
+                spanIndices.push({begins:[],ends:[]});
+            }
+
+            var idx = 0;
+
+            var n = text.length-1;
+
+            for(var keyName in spanss) {
+                if(!displayKeys[keyName]) {
+                    continue;
+                }
+                var spans = spanss[keyName];
+                for(var jdx in spans) {
+                    var span = spans[jdx];
+                    spanIndices[span.from].begins.push({color: colours[idx]});
+                    spanIndices[Math.min(n,span.to)].ends.push({color: colours[idx]});
+                }
+                ++idx;
+            }
+
+            var segments = [];
+
+            var prev = 0;
+
+            for(var i in spanIndices) {
+                var spansAtIdx = spanIndices[i];
+
+                if(spansAtIdx.begins.length || spansAtIdx.ends.length) {
+                    var segmentText = text.substring(prev, i);
+                    prev = i;
+
+                    segments.push({
+                        text: segmentText,
+                        begins : spansAtIdx.begins,
+                        ends : spansAtIdx.ends
+                    });
+                }
+
+            }
+
+            if(prev < text.length) {
+                segments.push({
+                    text: text.substring(prev, text.length),
+                    begins : [],
+                    ends : []
+                });
+            }
+
+            return segments;
+        }
+    }
+});
