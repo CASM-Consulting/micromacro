cc0784dbe8725bc4576d5696b78f3ad71aa77a01
Simon Wibberley <simon.wibberley@gmail.com>
2019-05-26 13:20:02 +0100

    added key selector code

---
+++ src/main/resources/assets/js/directives/collapseZone.js
@@ -0,0 +1,23 @@
+MicroMacroApp.directive("collapseZone", function () {
+    return {
+        restrict: "A",
+        scope: true,
+        link: function (scope, elem, attrs) {
+            scope._collapseZone = scope.$eval(attrs.collapseZone) || true;
+
+            scope.$watch(attrs.collapseZone, function (val) {
+                if (val) {
+                    scope._collapseZone = val;
+                }
+            });
+
+            if (attrs.collapseZoneId) {
+                scope.$watch(attrs.collapseZoneId, function (val) {
+                    if (val && typeof val === 'string') {
+                        scope._collapseZone._setZoneId(val);
+                    }
+                });
+            }
+        }
+    }
+});
