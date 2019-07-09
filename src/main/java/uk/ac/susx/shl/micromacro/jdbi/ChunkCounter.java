1856cd4923280b6a8cd9d91c3aa4af54640e779c
Simon Wibberley <simon.wibberley@gmail.com>
2019-07-09 11:30:58 +0100

    v1.2.1 added 'chunk counts' facility to report the number of chunks in each partition

--- src/main/java/uk/ac/susx/shl/micromacro/jdbi/ChunkCounter.java
+++ src/main/java/uk/ac/susx/shl/micromacro/jdbi/ChunkCounter.java
@@ -1,11 +1,13 @@
 package uk.ac.susx.shl.micromacro.jdbi;
 
 import com.google.gson.Gson;
+import uk.ac.susx.tag.method51.core.data.store2.query.Proximity;
 import uk.ac.susx.tag.method51.core.data.store2.query.SqlQuery;
 import uk.ac.susx.tag.method51.core.gson.GsonBuilderFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
@@ -21,6 +23,8 @@ public class ChunkCounter implements StreamFunction<SqlQuery, String> {
 
     public static final String ID2COUNT = "-id2Count";
     public static final String ROW_NUMER = "__row_number";
+    public static final String TARGET = "__target";
+    public static final String PROXIMITY = "__proximity";
     private final Gson gson = GsonBuilderFactory.get().create();
 
     public ChunkCounter(String key) {
@@ -32,9 +36,16 @@ public class ChunkCounter implements StreamFunction<SqlQuery, String> {
     @Override
     public Function<String, String> apply(SqlQuery query, Object obj) {
         CachingDAO<String, SqlQuery> cache = (CachingDAO<String, SqlQuery>)obj;
-        AtomicReference<String> partitionId = new AtomicReference<>("");
-        AtomicInteger i = new AtomicInteger(1);
+
+        AtomicReference<String> prevParition = new AtomicReference<>("");
+        AtomicInteger i = new AtomicInteger(0);
+        AtomicInteger j = new AtomicInteger(0);
         AtomicLong prevRowNumber = new AtomicLong(-1);
+
+//        int d = -1;
+//        if(query instanceof Proximity) {
+//            d = ((Proximity)query).distance();
+//        }
 
         String id = cache.getQueryId(query);
         Map<String, Integer> id2CountCache = cache.str2Int(id, ID2COUNT);
@@ -45,23 +56,34 @@ public class ChunkCounter implements StreamFunction<SqlQuery, String> {
             try {
 
                 long rowNumber = ((Double)data.get(ROW_NUMER)).longValue();
+                boolean target = (Boolean)data.get(TARGET);
+                boolean proximity = (Boolean)data.get(PROXIMITY);
 
                 String partition = getter.apply(data);
 
-                if(!partitionId.get().equals("")) {
+                boolean partitionEnd = !prevParition.get().equals("") && !prevParition.get().equals(partition);
+                boolean chunkEnd = prevRowNumber.get() != -1 && prevRowNumber.get() != rowNumber-1;
 
-                    if(!partition.equals(partitionId.get())) {
-
-                        id2CountCache.put(partition, i.get());
-                        LOG.info(partition + " " + i.get());
-                        prevRowNumber.set(-1);
-                        i.set(1);
-                    } else if(prevRowNumber.get() != -1 && rowNumber != prevRowNumber.get()+1){
-                        i.incrementAndGet();
-                    }
+                if(partitionEnd || chunkEnd) {
+                    i.addAndGet(j.get()-1);
                 }
-                prevRowNumber.set(rowNumber);
-                partitionId.set(partition);
+
+                if(partitionEnd) {
+                    id2CountCache.put(prevParition.get(), i.get());
+                    prevRowNumber.set(-1);
+                    i.set(0);
+                    j.set(0);
+                } else {
+                    prevRowNumber.set(rowNumber);
+                }
+                prevParition.set(partition);
+
+                if(proximity) {
+                    j.incrementAndGet();
+                }
+                if(target) {
+                    j.incrementAndGet();
+                }
 
             } catch (NullPointerException e) {
 
