14216a63dbd321333331e1a255a4a9d772379c1d
Simon Wibberley <simon.wibberley@gmail.com>
2019-08-19 10:27:28 +0100

    updated datum to __match_data ver.

--- src/main/java/uk/ac/susx/shl/micromacro/resources/QueryResource.java
+++ src/main/java/uk/ac/susx/shl/micromacro/resources/QueryResource.java
@@ -15,7 +15,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 
